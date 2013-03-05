9e58985c192a9b0bfc987e352bc27fffef2ebb30
Venugopal Mopidevi <hb.venugopal@hiddenbrains.in>
2013-03-05 14:40:59 +0530

    Smarty Template Engine

---
+++ frontend/views/site/index1.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * index.php
+ *
+ * @author: antonio ramirez <antonio@clevertech.biz>
+ * Date: 7/22/12
+ * Time: 8:30 PM
+ */
+?>
+<?php echo Yii::t('global', 'This is index Page...'); ?><br />
+<?php echo Yii::t('global', 'Hidden Brains'); ?>
+<br />
+<?php echo Yii::t('global', 'Admin'); ?>
